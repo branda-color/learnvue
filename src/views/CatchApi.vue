e95a499d495a07b7607cbb5dc8371e9aaa5ab32b
tzu <wishcolor0770@yahoo.com.tw>
2022-04-01 12:00:24 +0800

    串接後端api

---
+++ src/views/CatchApi.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="catch">
+    <h1>This is a catch page</h1>
+    <div>{{ info.to }}</div>
+    <div>{{ info.status }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "catch-data",
+  data() {
+    return {
+      text: "helloworld",
+      info: {},
+    };
+  },
+  async created() {
+    // GET request using fetch with async/await
+    const response = await fetch("https://uat.tickup.work/api/tovue");
+    const data = await response.json();
+    this.info = data;
+  },
+};
+</script>
